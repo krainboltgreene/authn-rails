f0d5a62ea5791856d73f35b0ff3cadcbf2ee5a00
krainboltgreene <me@kurtisrainboltgreene.name>
2012-08-29 10:32:56 -0500

    Adding the rails engine.

---
+++ lib/authn/rails/engine.rb
@@ -0,0 +1,13 @@
+module AuthN
+  # The AuthN engine takes care of extending ActiveRecord (if used) and ActionController,
+  # With the plugin logic.
+  class Engine < Rails::Engine
+    config.authn = AuthN.config
+
+    initializer "Extend ActionController with AuthN" do |app|
+      ActionController::Base.send :include, AuthN::Session
+      ActionController::Base.helper_method :current_user
+      ActionController::Base.helper_method :logged_in?
+    end
+  end
+end
---
+++ lib/authn/rails/version.rb
@@ -0,0 +1,5 @@
+module AuthN
+  module Rails
+    VERSION = "1.0.4"
+  end
+end
