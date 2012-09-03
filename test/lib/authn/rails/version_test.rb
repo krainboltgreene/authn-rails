ed1a6b41611d9899d689cc6e4e94af4440e95569
krainboltgreene <me@kurtisrainboltgreene.name>
2012-09-02 20:35:15 -0500

    Removing an unneeded dependency in the gemspec

--- test/lib/authn/rails/version_test.rb
+++ test/lib/authn/rails/version_test.rb
@@ -3,6 +3,6 @@ require_relative '../../../helper'
 
 class TestAuthNRailsVersion < MiniTest::Unit::TestCase
   def test_that_version_is_latest
-    assert_equal "1.3.0", AuthN::Rails::VERSION
+    assert_equal "1.3.1", AuthN::Rails::VERSION
   end
 end
